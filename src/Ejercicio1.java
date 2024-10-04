033d514eef436ece1ec9d69394854f561a993b47
Diego Mullo <diego.mullo@epn.edu.ec>
2024-10-04 12:51:21 -0500

    commit 1

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -4,13 +4,8 @@
 
 public class Ejercicio1 {
     public static void main(String[] args) {
+        System.out.println("Hola, mi nombre es Diego Mullo");
 
-        System.out.println("Hello and welcome babyyy!");
-        int edad = 18;
 
-        if(edad>=18){
-            System.out.println("Usted puede realizar el proceso electoral de votar.");
-        }//asdasd
-        //hbvfchvt
     }
 }
