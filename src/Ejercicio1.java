0b67a9f7b53fa8340c319a738eaaa836fd90d622
Diego Mullo <diego.mullo@epn.edu.ec>
2024-10-04 12:06:31 -0500

    asgdhasj

---
+++ src/Ejercicio1.java
@@ -0,0 +1,15 @@
+//Para una linea
+/* para varias lineas
+ * en este formato */
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome babyyy!");
+        int edad = 18;
+
+        if(edad>=18){
+            System.out.println("Usted puiede realizar el proceso electoral de votar.");
+        }//asdasd
+    }
+}
